7761f0be521ebf76d6ccbae398aaa2a5493622c6
Гордеев Игорь Юрьевич <IYGordeev@sberbank.ru>
2021-09-14 16:04:07 +0500

    get data weather

--- App.js
+++ App.js
@@ -1,17 +1,38 @@
 import { StatusBar } from "expo-status-bar"
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import * as Location from "expo-location"
-import { StyleSheet, Text, View } from "react-native"
+import { Alert, StyleSheet, Text, View } from "react-native"
+import axios from "axios"
 import Loading from "./Loading"
 
+const API_KEY = "c2d1935b0df91870ab15c09d3f83e0c0"
+
 export default function () {
+  const [loading, setLoading] = useState(true)
+
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+    )
+    console.log(data)
+  }
+
   getLocation = async () => {
-    const location = await Location.getCurrentPositionAsync()
-    console.log(location)
+    try {
+      await Location.requestForegroundPermissionsAsync()
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getLastKnownPositionAsync()
+      getWeather(latitude, longitude)
+      setLoading(false)
+      // сделать запрос к API
+    } catch (error) {
+      Alert.alert("Не могу определить местоположение", "Грустненько :(")
+    }
   }
   useEffect(() => {
     getLocation()
   }, [])
 
-  return <Loading />
+  return loading ? <Loading /> : null
 }
